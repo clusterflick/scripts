b5e6407bb2a347aa646dcdc822c15f7cd0f73742
Alistair Brown <github@alistairjcbrown.com>
2025-11-06 23:47:06 +0000

    Update title normalization test

--- common/normalize-title.js
+++ common/normalize-title.js
@@ -533,6 +533,12 @@ function normalizeTitle(title, options) {
     .replace(/^(-|–)/g, "")
     .replace(/(-|–|\()$/g, "")
     .replace(/!|:|\.|\*|…|—|]|<|>/g, " ")
+    .replaceAll("–", "–")
+    .replace(
+      // Remove emoji
+      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+      "",
+    )
     .replace(/\s+/g, " ")
     .replace(/^(.+),\s+the$/, "the $1")
     .trim()
