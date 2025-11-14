c82bb86ca4708f8c8df576d1eff66b9cdd3f368d
Alistair Brown <github@alistairjcbrown.com>
2025-11-14 00:28:47 +0000

    Add new venue

---
+++ cinemas/theexhibit.co.uk/attributes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  id: "theexhibit.co.uk",
+  name: "The Exhibit",
+  alternativeNames: ["Exhibit Bar & Restaurant"],
+  domain: "https://www.theexhibit.co.uk",
+  url: "https://www.theexhibit.co.uk/whatson/",
+  address: "12 Balham Station Road, London, SW12 9SG, UK",
+  geo: { lat: 51.4433611277788, lon: -0.1513853314514526 },
+};
