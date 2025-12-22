a72558f51eb76532cfbf4f983f28baa28b47d0c4
Alistair Brown <github@alistairjcbrown.com>
2025-12-22 23:18:55 +0100

    Remove football match data from boxpark

---
+++ cinemas/boxpark.co.uk-wembley/transform.js
@@ -0,0 +1,20 @@
+const { basicNormalize } = require("../../common/utils");
+
+const sportShowings = [
+  /\s+Cup Screening$/i,
+  /\s+Premier League Screening$/i,
+  /Union Jack Classic/i,
+  /Super Bowl/i,
+];
+
+const isNotSportShowing = ({ title }) =>
+  !sportShowings.some((sports) => basicNormalize(title).match(sports));
+
+async function transform(data, sourcedEvents) {
+  // Return the sourced events for this venue
+  return Object.values(sourcedEvents)
+    .flatMap((events) => events)
+    .filter(isNotSportShowing); // Remove all the football match screenings
+}
+
+module.exports = transform;
