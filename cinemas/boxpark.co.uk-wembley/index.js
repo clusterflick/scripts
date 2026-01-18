448fd9b32fa5aada58e7a8c830954c43c0704844
alistairjcbrown <635903+alistairjcbrown@users.noreply.github.com>
2026-01-18 09:17:26 +0000

    Update title normalization test

--- cinemas/boxpark.co.uk-wembley/index.js
+++ cinemas/boxpark.co.uk-wembley/index.js
@@ -1,6 +1,6 @@
 const attributes = require("./attributes");
 const retrieve = require("../../common/source-only/retrieve");
-const transform = require("./transform");
+const transform = require("../../common/source-only/transform");
 
 module.exports = {
   attributes,
--- common/source-only/transform.js
+++ common/source-only/transform.js
@@ -1,6 +1,22 @@
+const { basicNormalize } = require("../../common/utils");
+
+const sportShowings = [
+  /\s+Cup Screening$/i,
+  /\s+League Screening$/i,
+  /Union Jack Classic/i,
+  /Super Bowl/i,
+  /Six Nations/i,
+  /AFCON\s+/i,
+];
+
+const isNotSportShowing = ({ title }) =>
+  !sportShowings.some((sports) => basicNormalize(title).match(sports));
+
 async function transform(data, sourcedEvents) {
   // Return the sourced events for this venue
-  return Object.values(sourcedEvents).flatMap((events) => events);
+  return Object.values(sourcedEvents)
+    .flatMap((events) => events)
+    .filter(isNotSportShowing); // Remove all the football match screenings
 }
 
 module.exports = transform;
