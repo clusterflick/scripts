03d857da644177dcb3832edf9cbf8e49c3369874
Alistair Brown <github@alistairjcbrown.com>
2026-01-19 01:28:46 +0000

    Update test for correct movie pages length

--- sources/eventbrite.co.uk/tests/index.test.js
+++ sources/eventbrite.co.uk/tests/index.test.js
@@ -35,7 +35,7 @@ describe(attributes.name, () => {
       expect(movieListPages).toBeTruthy();
       expect(movieListPages).toHaveLength(60);
       expect(moviePages).toBeTruthy();
-      expect(Object.keys(moviePages)).toHaveLength(929);
+      expect(Object.keys(moviePages)).toHaveLength(939);
 
       readJSON.mockImplementation(() => ({ movieListPages, moviePages }));
 
