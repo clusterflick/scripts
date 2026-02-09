25bba74934cd5d0f2fcb9c4e12801b8d7300fa38
Alistair Brown <github@alistairjcbrown.com>
2026-02-09 20:01:18 +0000

    Rename venue

--- cinemas/london.czechcentres.cz/attributes.js
+++ cinemas/london.czechcentres.cz/attributes.js
@@ -1,18 +1,18 @@
 module.exports = {
-  id: "mzv.gov.cz-london",
+  id: "london.czechcentres.cz",
   name: "Czech Centre",
   alternativeNames: [
     "Czech Centre London",
     "Czech Centre at the Czech Embassy Cinema",
     "Czech Embassy Cinema",
   ],
-  domain: "https://mzv.gov.cz",
+  domain: "https://london.czechcentres.cz",
   socials: {
     letterboxd: null,
     twitter: "CzechLondon",
     instagram: "czinlondon",
   },
-  url: "https://mzv.gov.cz/london/cz/o_velvyslanectvi/kontaktni_informace/ceske_centrum_londyn.html",
+  url: "https://london.czechcentres.cz/en/program?locale=en&category=film",
   address: "30 Kensington Palace Gardens, London, W8 4QY, UK",
   geo: { lat: 51.50937215348203, lon: -0.19390184579204625 },
   structure: "solo",
