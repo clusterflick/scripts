82400ce3de2f2a29f974354f19a470dbd8ea87a1
Alistair Brown <github@alistairjcbrown.com>
2026-02-24 19:29:30 +0000

    Remove odeon.co.uk-camden

--- cinemas/odeon.co.uk-camden/retrieve.js
+++
@@ -1,8 +0,0 @@
-const attributes = require("./attributes");
-const odeonRetrieve = require("../../common/odeon.co.uk/retrieve");
-
-async function retrieve() {
-  return odeonRetrieve(attributes);
-}
-
-module.exports = retrieve;
--- cinemas/odeon.co.uk-camden/transform.js
+++
@@ -1,8 +0,0 @@
-const attributes = require("./attributes");
-const odeonTransform = require("../../common/odeon.co.uk/transform");
-
-async function transform(data, sourcedEvents) {
-  return odeonTransform(attributes, data, sourcedEvents);
-}
-
-module.exports = transform;
