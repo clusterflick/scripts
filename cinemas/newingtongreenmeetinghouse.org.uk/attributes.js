d007e33ebaf53e670aef186d5d199376a43795a0
Alistair Brown <github@alistairjcbrown.com>
2026-03-11 21:34:59 +0000

    Add new venue

---
+++ cinemas/newingtongreenmeetinghouse.org.uk/attributes.js
@@ -0,0 +1,15 @@
+module.exports = {
+  id: "newingtongreenmeetinghouse.org.uk",
+  name: "Newington Green Meeting House",
+  domain: "https://newingtongreenmeetinghouse.org.uk",
+  socials: {
+    letterboxd: null,
+    twitter: "NGMeetingHouse",
+    instagram: "ngmeetinghouse",
+  },
+  url: "https://newingtongreenmeetinghouse.org.uk",
+  address: "39a Newington Green, London, N16 9PR, UK",
+  geo: { lat: 51.55217153776038, lon: -0.08491191519282906 },
+  structure: "solo",
+  type: "Community Cinema",
+};
