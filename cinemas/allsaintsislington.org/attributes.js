6ca340a46e903e10a06a76c2d543202822ac87e3
Alistair Brown <github@alistairjcbrown.com>
2026-03-15 21:55:39 +0000

    Add new venue

---
+++ cinemas/allsaintsislington.org/attributes.js
@@ -0,0 +1,16 @@
+module.exports = {
+  id: "allsaintsislington.org",
+  name: "All Saints Church",
+  alternativeNames: ["All Saints Caledonian Road", "All Saints"],
+  domain: "https://www.allsaintsislington.org",
+  socials: {
+    letterboxd: null,
+    twitter: null,
+    instagram: null,
+  },
+  url: "https://www.allsaintsislington.org",
+  address: "All Saints Church, Carnegie Street, London, N1 9QW, UK",
+  geo: { lat: 51.53545360821759, lon: -0.11661864069975249 },
+  structure: "solo",
+  type: "Community Cinema",
+};
