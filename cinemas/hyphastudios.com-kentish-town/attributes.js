075d8115c19b96daa083fee5fb8fbda96da31454
Alistair Brown <github@alistairjcbrown.com>
2026-03-16 13:09:23 +0000

    Add new venue

---
+++ cinemas/hyphastudios.com-kentish-town/attributes.js
@@ -0,0 +1,16 @@
+module.exports = {
+  id: "hyphastudios.com-kentish-town",
+  name: "Hypha Studio Kentish Town",
+  alternativeNames: ["Hypha Studios", "2 Prince of Wales Road"],
+  domain: "https://hyphastudios.com",
+  socials: {
+    letterboxd: null,
+    twitter: "HyphaStudios",
+    instagram: "hyphastudios",
+  },
+  url: "https://hyphastudios.com/kentish-town/",
+  address: "2 Prince of Wales Road, London, NW5 3LQ, UK",
+  geo: { lat: 51.546643126391835, lon: -0.142178867877304 },
+  structure: "solo",
+  type: "Community Cinema",
+};
