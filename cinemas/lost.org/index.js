fd871bb43cfeb1fb24757adbd7c9bf96c642eed3
Alistair Brown <github@alistairjcbrown.com>
2026-06-11 20:13:43 +0100

    Remove venue

--- cinemas/lost.org/index.js
+++
@@ -1,9 +0,0 @@
-const attributes = require("./attributes");
-const retrieve = require("../../common/source-only/retrieve");
-const transform = require("../../common/source-only/transform");
-
-module.exports = {
-  attributes,
-  retrieve,
-  transform,
-};
