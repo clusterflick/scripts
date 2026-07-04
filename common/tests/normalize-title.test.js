e3b7e77edae11851dcf0b282e4804c741c9fb871
alistairjcbrown <635903+alistairjcbrown@users.noreply.github.com>
2026-07-04 17:12:09 +0000

    Update title normalization test

--- common/tests/normalize-title.test.js
+++ common/tests/normalize-title.test.js
@@ -2,8 +2,6 @@ const normalizeTitle = require("../normalize-title");
 const testTitles = require("./test-titles.json");
 
 describe("Normalise Title", () => {
-  jest.useFakeTimers().setSystemTime(new Date("2025-06-15"));
-
   test.each(testTitles)(
     "normalizes the title '$input'",
     ({ input, output }) => {
