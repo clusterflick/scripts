1997e696dd88df458a20affcc4954d2c0f60a24d
Alistair Brown <github@alistairjcbrown.com>
2026-07-11 20:06:11 +0100

    Add new venue

---
+++ cinemas/stmaryswalthamstow.org/attributes.js
@@ -0,0 +1,16 @@
+module.exports = {
+  id: "stmaryswalthamstow.org",
+  name: "St Mary's Church Walthamstow",
+  alternativeNames: ["St Mary's Church"],
+  domain: "https://www.stmaryswalthamstow.org",
+  socials: {
+    letterboxd: null,
+    twitter: null,
+    instagram: "stmaryswalthamstow",
+  },
+  url: "https://www.stmaryswalthamstow.org",
+  address: "8 Church End, London, E17 9RJ, UK",
+  geo: { lat: 51.58507183328543, lon: -0.011947044051369687 },
+  structure: "solo",
+  type: "Community Centre",
+};
