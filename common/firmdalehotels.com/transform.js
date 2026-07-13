c785bf796bb21ca295b1e16bdcc465a8d9f757c5
Alistair Brown <github@alistairjcbrown.com>
2026-07-13 21:02:50 +0100

    Remove no movie check from firmdale hotels

--- common/firmdalehotels.com/transform.js
+++ common/firmdalehotels.com/transform.js
@@ -116,10 +116,6 @@ async function transform(attributes, { movieListPage }, sourcedEvents) {
     });
   });
 
-  if (movies.length === 0) {
-    throw new Error("No movies found - the page structure may have changed");
-  }
-
   const listOfSourcedEvents = Object.values(sourcedEvents).flatMap(
     (events) => events,
   );
