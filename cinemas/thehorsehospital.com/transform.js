add6c85f09042b44c58c8f1d7398b113d206e607
Alistair Brown <github@alistairjcbrown.com>
2026-07-17 10:16:00 +0100

    Allow no events at the Horse Hospital

--- cinemas/thehorsehospital.com/transform.js
+++ cinemas/thehorsehospital.com/transform.js
@@ -58,10 +58,6 @@ async function transform({ moviePages }, sourcedEvents) {
     });
   }
 
-  if (movies.length === 0) {
-    throw new Error("No movies found - the page structure may have changed");
-  }
-
   const listOfSourcedEvents = Object.values(sourcedEvents).flatMap(
     (events) => events,
   );
