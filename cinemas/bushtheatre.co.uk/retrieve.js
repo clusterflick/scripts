f33f325b3b9303d660fbd0497b7b3048463578c8
Alistair Brown <github@alistairjcbrown.com>
2026-08-02 20:44:07 +0200

    Skip Bush Theatre when it's returning 500

--- cinemas/bushtheatre.co.uk/retrieve.js
+++ cinemas/bushtheatre.co.uk/retrieve.js
@@ -3,25 +3,29 @@ const attributes = require("./attributes");
 const { fetchText, assertSelector } = require("../../common/utils");
 
 async function retrieve() {
-  const eventListPage = await fetchText(attributes.url);
-  assertSelector(eventListPage, ".events_listing .events_item__link");
-  const $ = cheerio.load(eventListPage);
+  try {
+    const eventListPage = await fetchText(attributes.url);
+    assertSelector(eventListPage, ".events_listing .events_item__link");
+    const $ = cheerio.load(eventListPage);
 
-  const eventPageUrls = new Set();
-  $(".events_listing .events_item__link").each(function () {
-    const href = $(this).attr("href");
-    if (href) eventPageUrls.add(new URL(href, attributes.url).href);
-  });
+    const eventPageUrls = new Set();
+    $(".events_listing .events_item__link").each(function () {
+      const href = $(this).attr("href");
+      if (href) eventPageUrls.add(new URL(href, attributes.url).href);
+    });
 
-  const eventPages = [];
-  for (const eventPageUrl of eventPageUrls) {
-    eventPages.push(await fetchText(eventPageUrl));
+    const eventPages = [];
+    for (const eventPageUrl of eventPageUrls) {
+      eventPages.push(await fetchText(eventPageUrl));
+    }
+
+    return {
+      eventListPage,
+      eventPages,
+    };
+  } catch {
+    return { eventListPage: "", eventPages: [] };
   }
-
-  return {
-    eventListPage,
-    eventPages,
-  };
 }
 
 module.exports = retrieve;
