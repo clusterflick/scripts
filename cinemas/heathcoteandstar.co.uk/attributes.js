f599ad5f251eeefa6234b8df846ff6502b6ad7ea
Alistair Brown <github@alistairjcbrown.com>
2026-08-07 14:17:16 +0100

    Add new venue

---
+++ cinemas/heathcoteandstar.co.uk/attributes.js
@@ -0,0 +1,16 @@
+module.exports = {
+  id: "heathcoteandstar.co.uk",
+  name: "Heathcote & Star",
+  alternativeNames: ["Heathcote and Star"],
+  domain: "https://heathcoteandstar.co.uk",
+  socials: {
+    letterboxd: null,
+    twitter: "heathcotestar",
+    instagram: "heathcotestar",
+  },
+  url: "https://heathcoteandstar.co.uk/whats-on/",
+  address: "344 Grove Green Road, London, E11 4EA, UK",
+  geo: { lat: 51.56599131726399, lon: 0.0022344424557753594 },
+  structure: "solo",
+  type: "Bar",
+};
