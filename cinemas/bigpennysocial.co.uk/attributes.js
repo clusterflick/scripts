a06cc5246fdb165d4e63d5cf95f11c4cd2f6d21b
Alistair Brown <github@alistairjcbrown.com>
2026-08-21 01:50:41 +0100

    Add new venue

---
+++ cinemas/bigpennysocial.co.uk/attributes.js
@@ -0,0 +1,15 @@
+module.exports = {
+  id: "bigpennysocial.co.uk",
+  name: "Big Penny Social",
+  domain: "https://bigpennysocial.co.uk",
+  socials: {
+    letterboxd: null,
+    twitter: "BigPennySocial",
+    instagram: "bigpennysocial",
+  },
+  url: "https://bigpennysocial.co.uk/whats-on",
+  address: "1 Priestley Way, London, E17 6AL, UK",
+  geo: { lat: 51.59102170988931, lon: -0.041318101723084824 },
+  structure: "solo",
+  type: "Bar",
+};
