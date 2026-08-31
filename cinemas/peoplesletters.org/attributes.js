34acce434db4729a890b372e2391740735151cc6
Alistair Brown <github@alistairjcbrown.com>
2026-08-31 15:53:51 +0100

    Add new venue

---
+++ cinemas/peoplesletters.org/attributes.js
@@ -0,0 +1,17 @@
+module.exports = {
+  id: "peoplesletters.org",
+  name: "The People's Letters Bookshop",
+  alternativeNames: ["Co-operative bookshop"],
+  domain: "https://peoplesletters.org",
+  socials: {
+    letterboxd: null,
+    twitter: "PeoplesLetters",
+    instagram: "peoplesletters",
+  },
+  url: "https://peoplesletters.org/events",
+  address: "395 Cambridge Heath Road, Bethnal Green, London, E2 9RA, UK",
+  geo: { lat: 51.53076013335542, lon: -0.05642618638148895 },
+  structure: "solo",
+  type: "Shop",
+  programming: "host",
+};
